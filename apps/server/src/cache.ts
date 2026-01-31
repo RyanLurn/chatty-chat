f054e5e2cd10fc23b13cd934c5ac4c6141aa2025
Ryan Luong <lumintriet@gmail.com>
2026-01-31 22:22:33 +0700

    feat(server): remove old schema, rewrite cache.

--- apps/server/src/cache.ts
+++ apps/server/src/cache.ts
@@ -1,3 +1,19 @@
-import type { ChatMessage, ChatMessageId } from "#schema";
+import type {
+  ChatMessage,
+  ChatMessageId,
+  ChatMessageReader,
+  ChatMessageReaderId,
+} from "#data/schema/chat-message";
+import type { User, UserId } from "#data/schema/user";
 
-export const cache: Map<ChatMessageId, ChatMessage> = new Map();
+export interface Cache {
+  users: Map<UserId, User>;
+  chatMessages: Map<ChatMessageId, ChatMessage>;
+  chatMessageReaders: Map<ChatMessageReaderId, ChatMessageReader>;
+}
+
+export const cache: Cache = {
+  users: new Map(),
+  chatMessages: new Map(),
+  chatMessageReaders: new Map(),
+};
