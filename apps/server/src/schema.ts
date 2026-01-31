5839c3e8385b2958e47c5725068cc041eb3f9413
Ryan Luong <lumintriet@gmail.com>
2026-01-31 21:57:10 +0700

    feat(server): add id for chat message schema.

--- apps/server/src/schema.ts
+++ apps/server/src/schema.ts
@@ -1,6 +1,18 @@
+import type { Branded } from "@chatty-chat/utils/types";
 import * as z from "zod";
 
+export type ChatMessageId = Branded<string, "ChatMessageId">;
+
+export const ChatMessageIdSchema = z.custom<ChatMessageId>((value) => {
+  const validationResult = z.uuidv4().safeParse(value);
+  if (!validationResult.success) {
+    return false;
+  }
+  return true;
+});
+
 export const ChatMessageSchema = z.object({
+  id: ChatMessageIdSchema,
   sender: z.string().min(1).max(16),
   content: z.string().min(1).max(2048),
   timestamp: z.int().positive(),
