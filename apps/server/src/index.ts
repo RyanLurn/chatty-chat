48c629a89699d1400eae2f32504a879a7a7a60da
Ryan Luong <lumintriet@gmail.com>
2026-01-31 22:02:52 +0700

    feat(server): websocket server boilerplate.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const server = Bun.serve({
+  fetch(req, server) {
+    if (server.upgrade(req)) {
+      return;
+    }
+    return new Response("Upgrade failed", { status: 500 });
+  },
+  websocket: {}, // handlers
+});
+
+console.log(`Server running at ${server.url}`);
